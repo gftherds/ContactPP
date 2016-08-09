35a00e9ccda7da922bebc204d766e713448a5d3a
Nutdanai Prom <Nutdanai.p@augmentis.biz>
2016-08-09 16:41:05 +0700

    Contact_Single

--- app/src/main/java/com/example/therdsak/contectpp/Activity/ContactListActivity.java
+++ app/src/main/java/com/example/therdsak/contectpp/Activity/ContactListActivity.java
@@ -1,7 +1,34 @@
 package com.example.therdsak.contectpp.Activity;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+
+import com.example.therdsak.contectpp.Model.Contact;
+
 /**
  * Created by Therdsak on 8/9/2016.
  */
-public class ContactListActivity {
+public class ContactListActivity extends Fragment {
+    private CallBack callBack;
+
+
+    protected interface CallBack{
+        void onContactSelect(Contact contact);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        callBack = (CallBack) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        callBack = null ;
+    }
+
+
+
+    
 }
