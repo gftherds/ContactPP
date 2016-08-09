f9000c339b82ec2a3ef8e510d99f1025abc49279
Nutdanai Prom <Nutdanai.p@augmentis.biz>
2016-08-09 17:30:26 +0700

    Contact_Edit_Activity

--- app/src/main/java/com/example/therdsak/contectpp/Activity/ContactActivity.java
+++ app/src/main/java/com/example/therdsak/contectpp/Activity/ContactActivity.java
@@ -1,7 +1,21 @@
 package com.example.therdsak.contectpp.Activity;
 
+
+import android.support.v4.app.Fragment;
+
+import com.example.therdsak.contectpp.Model.Contact;
+
 /**
  * Created by Therdsak on 8/9/2016.
  */
-public class ContactActivity {
+public class ContactActivity extends SingleFragmentActivity implements ContactListActivity.CallBack {
+    @Override
+    protected Fragment onCreateFragment() {
+        return new ContactListFragment();
+    }
+
+    @Override
+    public void onContactSelect(Contact contact) {
+        //
+    }
 }
