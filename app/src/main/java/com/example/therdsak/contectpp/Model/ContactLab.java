d2dede9068bc4f0bdb3d83ac1dda2be3c12b5624
outnotin <outnotin@gmail.com>
2016-08-09 14:27:13 +0700

    test commit model

--- app/src/main/java/com/example/therdsak/contectpp/Model/ContactLab.java
+++ app/src/main/java/com/example/therdsak/contectpp/Model/ContactLab.java
@@ -1,7 +1,61 @@
 package com.example.therdsak.contectpp.Model;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.CursorWrapper;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.therdsak.contectpp.DataBase.ContactCursorWrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Therdsak on 8/9/2016.
  */
 public class ContactLab {
+
+    private static ContactLab instance;
+
+    public static ContactLab getInstance(Context context){
+        if(instance == null){
+            instance = new ContactLab(context);
+        }
+        return instance;
+    }
+
+    public static ContentValues getContentValues(Contact contact){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Conta);
+    }
+
+    private Context context;
+    private SQLiteDatabase database;
+
+    private ContactLab(Context context){
+        this.context = context.getApplicationContext();
+    }
+
+    public ContactCursorWrapper queryCrimes(String whereClause, String[] whereArgs){
+        Cursor cursor = database.query(null, null, whereClause, whereArgs,null,
+                null,null);
+        return new ContactCursorWrapper();
+    }
+
+    public List<Contact> getContacts(){
+        List<Contact> contacts = new ArrayList<>();
+        ContactCursorWrapper cursor = queryCrimes(null, null);
+
+        try{
+
+        }finally {
+
+        }
+        return contacts;
+    }
+
+    public void addCrime(Contact contact){
+        ContentValues contentValues = getConten
+    }
 }
